bf34c54b080992b72e381bf9c5463815f117d499
jourdiw <jourdiwaller@gmail.com>
2020-10-07 18:45:15 +0200

    [Back] Added Swagger and simplified how the 'name' query parameter is passed to functions.

--- spring-boot/src/main/java/com/hero/controller/HeroController.java
+++ spring-boot/src/main/java/com/hero/controller/HeroController.java
@@ -2,7 +2,7 @@ package com.hero.controller;
 
 import com.hero.model.Hero;
 import com.hero.repository.HeroRepository;
-import com.hero.exception.HeroNotFoundException;
+import io.swagger.annotations.ApiParam;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -31,39 +30,42 @@ public class HeroController {
     }
 
     /**
-     * Look up all employees, and transform them into a REST collection resource. Then return them through Spring Web's
+     * Look up all heroes, and transform them into a REST collection resource. Then return them through Spring Web's
      * {@link ResponseEntity} fluent API.
+     * @param name is an optional query that is used by the search function in the front-end
+     * @return
      */
 
     @GetMapping("/heroes")
-    ResponseEntity<CollectionModel<EntityModel<Hero>>> all(@RequestParam Map<String,String> searchParams) {
+    ResponseEntity<CollectionModel<EntityModel<Hero>>> all(@ApiParam(name="name", value = "Search for Hero's name")
+            @RequestParam(value="name", required = false) String name) {
 
-        List<Hero> heroesList;
-
-        if (searchParams.containsKey("name")) {
-            String nameQuery = searchParams.get("name");
-            heroesList = repository.findByNameContainingIgnoreCase(nameQuery);
-        } else {
-            heroesList = repository.findAll();
-        }
+        List<Hero> heroesList = name != null ?
+                repository.findByNameContainingIgnoreCase(name)
+                : repository.findAll();
 
         List<EntityModel<Hero>> heroes = StreamSupport.stream(heroesList.spliterator(), false)
                 .map(hero -> new EntityModel<>(hero,
-                        linkTo(methodOn(HeroController.class).one(hero.getId(), searchParams)).withSelfRel().expand(),
-                        linkTo(methodOn(HeroController.class).all(searchParams)).withRel("heroes").expand()))
+                        linkTo(methodOn(HeroController.class).one(hero.getId())).withSelfRel().expand(),
+                        linkTo(methodOn(HeroController.class).all(name)).withRel("heroes").expand()))
                 .collect(Collectors.toList());
 
         return ResponseEntity.ok(new CollectionModel<>(heroes,
-                linkTo(methodOn(HeroController.class).all(searchParams)).withSelfRel()));
+                linkTo(methodOn(HeroController.class).all(name)).withSelfRel()));
     }
 
+    /**
+     * Add a new hero with an object, only name is required in the Hero object
+     * @param newHero
+     * @return
+     */
     @PostMapping("/heroes")
-    ResponseEntity newHero(@RequestBody Hero newHero, @RequestParam Map<String,String> searchParams) {
+    ResponseEntity newHero(@RequestBody Hero newHero) {
         try {
             Hero savedHero = repository.save(newHero);
 
             EntityModel<Hero> heroResource = new EntityModel<>(savedHero,
-                    linkTo(methodOn(HeroController.class).one(savedHero.getId(), searchParams)).withSelfRel());
+                    linkTo(methodOn(HeroController.class).one(savedHero.getId())).withSelfRel());
             return  ResponseEntity.created(new URI(heroResource.getRequiredLink(IanaLinkRelations.SELF).getHref()))
                     .body(heroResource);
         } catch (URISyntaxException e) {
@@ -74,36 +76,33 @@ public class HeroController {
     /**
      * Look up a single {@link Hero} and transform it into a REST resource. Then return it through Spring Web's
      * {@link ResponseEntity} fluent API.
-     *
      * @param id
      */
 
     @GetMapping("/heroes/{id}")
-    ResponseEntity<EntityModel<Hero>> one(@PathVariable long id, @RequestParam Map<String,String> searchParams) {
-        System.out.println("[one]" + id);
+    ResponseEntity<EntityModel<Hero>> one(@PathVariable long id) {
         return repository.findById(id)
                 .map(hero -> new EntityModel<>(hero,
-                        linkTo(methodOn(HeroController.class).one(hero.getId(), searchParams)).withSelfRel(),
-                        linkTo(methodOn(HeroController.class).all(searchParams)).withRel("heroes")))
+                        linkTo(methodOn(HeroController.class).one(hero.getId())).withSelfRel(),
+                        linkTo(methodOn(HeroController.class).all("")).withRel("heroes")))
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     /**
-     * Update existing employee then return a Location header.
-     *
+     * Update existing hero then return a Location header.
      * @param newHero
      * @param id
      * @return
      */
     @PutMapping("/heroes/{id}")
-    ResponseEntity<?> updateHero(@RequestBody Hero newHero, @PathVariable long id, @RequestParam Map<String,String> searchParams) {
+    ResponseEntity<?> updateHero(@RequestBody Hero newHero, @PathVariable long id) {
 
         Hero heroToUpdate = newHero;
         heroToUpdate.setId(id);
         repository.save(heroToUpdate);
 
-        Link newlyCreatedLink = linkTo(methodOn(HeroController.class).one(id, searchParams)).withSelfRel();
+        Link newlyCreatedLink = linkTo(methodOn(HeroController.class).one(id)).withSelfRel();
 
         try {
             return ResponseEntity.noContent().location(new URI(newlyCreatedLink.getHref())).build();
@@ -112,6 +111,11 @@ public class HeroController {
         }
     }
 
+    /**
+     * Delete a hero
+     * @param id
+     * @return
+     */
     @DeleteMapping("/heroes/{id}")
     ResponseEntity<?> deleteHero(@PathVariable long id) {
 
